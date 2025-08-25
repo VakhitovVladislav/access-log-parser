ebaeb5b16893cd5b77431d4e8aab16121a18f99a
Vlad <siemiens11@gmail.com>
2025-08-25 23:17:23 +0800

    -add new methods in Main -add pom.xml -fix gitignore -add MANIFEST files

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+package main.java;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Enter text and click <Enter>: ");
+        String text = new Scanner(System.in).nextLine();
+        System.out.println("Text length: " + text.length());
+    }
+}
