0eeb8582db30137199548372a1e941153789ef4e
Vlad <siemiens11@gmail.com>
2025-08-31 12:54:30 +0800

    add new main with homework quotinent of two numbers

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Enter text and click <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Text length: " + text.length());
+        // Получаем первое число
+        System.out.println("Please enter you first Number");
+        int firstNumber = new Scanner(System.in).nextInt();
+        // Получаем второе число
+        System.out.println("Please enter you second Number");
+        int secondNumber = new Scanner(System.in).nextInt();
+        // Считаем частное
+        double quotient = (double) firstNumber / secondNumber;
+        // Считаем сумму
+        int sum = firstNumber + secondNumber;
+        // Считаем произведение
+        int product = firstNumber * secondNumber;
+        // Считаем разность
+        int difference=  firstNumber - secondNumber;
+        // Выводим на консоль результаты
+        System.out.println("quotient is: " + quotient);
+        System.out.println("sum is: " + sum);
+        System.out.println("product of numbers  is: " + product);
+        System.out.println("number difference is: " + difference);
+
+
     }
 }
