0a05292b48929b89bd7cb21f92fe3b8d7192103e
Vlad <siemiens11@gmail.com>
2025-09-12 14:46:52 +0800

    -add new methods in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,26 @@
 package main.java;
 
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Enter text and click <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Text length: " + text.length());
+        int tryCount =0;
+        while (true){
+            String path = new Scanner(System.in).nextLine();
+
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean  isDirectory = file.isDirectory();
+            if (isDirectory){
+                System.out.println("it is a directory");
+            } else if (fileExists) {
+                System.out.println("The path is correct");
+                tryCount++;
+                System.out.println("The file is " + tryCount);
+            }else {
+                System.out.println("file is not exists or it is ");
+            }
+        }
     }
 }
